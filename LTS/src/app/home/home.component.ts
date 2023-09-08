5239a0e84d1cb023b47c14be14f9fbca586c91bd
Charankasa07 <s180067@rguktsklm.ac.in>
2023-09-08 10:29:02 +0530

    08-09-2023

--- LTS/src/app/home/home.component.ts
+++ LTS/src/app/home/home.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+import { UserRegister } from '../User';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
-
+export class HomeComponent implements OnInit{
+  constructor(private cookieService : CookieService){}
+  currentUser : UserRegister = {
+    role:'',
+    name:'',
+    mobile:'',
+    email:'',
+    password:'',
+    leaves:[],
+    numberOfLeaves:0
+  }
+  ngOnInit(): void {
+      let currentUser = this.cookieService.get('currentUser')
+      if(currentUser){
+        this.currentUser = JSON.parse(currentUser)
+        if(this.currentUser.role==='employee'){
+          window.location.href='http://localhost:4200/employee'
+        }
+        else{
+          window.location.href='http://locahost:4200/manager'
+        }
+      }
+      
+  }
 }
